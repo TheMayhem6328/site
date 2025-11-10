9d7628d840963c149c24b23c52f8d9440c130e1f
Zahiruzzaman Chowdhury <nebrus.gamer@gmail.com>
2025-11-10 17:25:23 +0600

    CSS Fix + Addition of content

--- script/main.js
+++ script/main.js
@@ -1,22 +1,34 @@
 const heroElement = document.getElementById("hero");
 const headerElement = document.getElementById("header");
+var cssScrollSupport = false;
 
-window.addEventListener('scroll', () => {
-    const start = 0;
-    const end = 1090;
-    const scrollY = window.scrollY;
+if (CSS.supports('animation-timeline', 'scroll()')) {
+    console.info('CSS function scroll() is supported in this browser. Will use CSS scroll animation once implemented');
+    // Set to true to try to attempt CSS based animation again
+    // Maybe edit above console.info() to reflect changes once done
+    cssScrollSupport = false;
+} else {
+    console.warn('CSS function scroll() is not supported in this browser. Using JS-based scroll animation');
+}
 
-    // Normalize scroll progress
-    const progress = Math.min(Math.max((scrollY - start) / (end - start), 0), 1);
+if (!cssScrollSupport) {
+    window.addEventListener('scroll', () => {
+        const start = 0;
+        const end = window.innerHeight;
+        const scrollY = window.scrollY;
 
-    const scale = 1 - 0.3 * progress;
-    const opacity = 1 - progress;
-    const blurAmount = Math.floor(25 * progress)
+        // Normalize scroll progress
+        const progress = Math.min(Math.max((scrollY - start) / (end - start), 0), 1);
 
-    heroElement.style.transform = `scale(${scale})`;
-    heroElement.style.opacity = opacity;
-    headerElement.style.filter = `blur(${blurAmount}px)`;
-});
+        const scale = 1 - 0.3 * progress;
+        const opacity = 1 - progress;
+        const blurAmount = Math.floor(25 * progress)
+
+        heroElement.style.transform = `scale(${scale})`;
+        heroElement.style.opacity = opacity;
+        headerElement.style.filter = `blur(${blurAmount}px)`;
+    });
+}
 
 document.getElementById("down-arrow").addEventListener('click', () => {
     const scrollY = window.innerHeight
